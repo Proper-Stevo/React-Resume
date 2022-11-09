15f0e09e9d4d1905a60370de24374e7c56bf4e02
Stevo <steven.barrios92@gmail.com>
2022-11-08 18:42:03 -0800

    contact info and about me almost done

--- src/components/Aboutme.js
+++ src/components/Aboutme.js
@@ -8,9 +8,9 @@ function About() {
       <h3>About Steven Barrios</h3>
       <div className="row">
         <img src={myphoto} className="col-12 col-md-3" id="selfie" />
-        <Accordion defaultActiveKey="0" className="col-12 col-md-9">
+        <Accordion defaultActiveKey="0" className=" col-12 col-md-9">
           <Accordion.Item eventKey="0">
-            <Accordion.Header>About Me</Accordion.Header>
+            <Accordion.Header >About Me</Accordion.Header>
             <Accordion.Body>
               A Full Stack Web Developer passionate about creating interactive applications
               and experiences on the internet. I love exploring new frameworks and technologies
@@ -35,10 +35,10 @@ function About() {
             </Accordion.Body>
           </Accordion.Item>
           <Accordion.Item eventKey="1">
-            <Accordion.Header>My Education</Accordion.Header>
+            <Accordion.Header >My Education</Accordion.Header>
             <Accordion.Body>
               UCLA Extension: Full Stack Developer Program. 
-              I learned so many languages and frameworks. Some of my favorite langues and frameworks include HTML, Node.Js, CSS, JavaScript, 
+              Fluent in numerous languages and frameworks including ("but not limited to"): HTML, Node.Js, CSS, JavaScript, 
               React, and many more. If you're interested in a passionate and motivated developer feel free to contact me!
             </Accordion.Body>
           </Accordion.Item>
--- src/components/ContactInfo.js
+++ src/components/ContactInfo.js
@@ -1,10 +1,64 @@
 import { Parallax } from 'react-parallax';
 import firecamp from '../img/backgroundThree 2.jpg'
+import env from '../img/email.png'
 
 const ContactInfo = () => (
-    <Parallax className='image3' bgImage={firecamp}  strength={800}>
-        <div className='content' id='contact'>
-            <span className='img-txt'>Hello A Message Goes Here</span>
+    <Parallax className='image3' bgImage={firecamp} strength={800}>
+        <div className="contact-content" id="contact">
+            <div className="contact-text">
+                <h1 className="text-center"><img src={env} className="env"/>   Contact Me</h1>
+                <form className="px-5 py-1">
+                    <div className="form-group ">
+                        <label for="name" className="py-1 ">
+                            Name:
+                        </label>
+                        <input
+                            value={"name"}
+                            name="name"
+                            onChange={"handleInputChange"}
+                            type="text"
+                            placeholder="name"
+                            class="form-control"
+                        />
+                    </div>
+
+                    <div className="form-group">
+                        <label for="email" className="py-1 ">
+                            Email address:
+                        </label>
+                        <input
+                            value={"email"}
+                            name="email"
+                            onChange={"handleInputChange"}
+                            type="email"
+                            placeholder="email"
+                            class="form-control"
+                        />
+                    </div>
+
+                    <div className="form-group">
+                        <label for="message" className="py-1 ">
+                            Message:
+                        </label>
+                        <textarea
+                            value={"message"}
+                            name="message"
+                            onChange={"handleInputChange"}
+                            type="text"
+                            placeholder="message"
+                            class="form-control"
+                            rows="3"
+                        />
+                    </div>
+                    <button
+                        type="submit"
+                        onClick={"handleFormSubmit"}
+                        class="btn btn-primary mt-2"
+                    >
+                        Submit
+                    </button>
+                </form>
+            </div>
         </div>
     </Parallax>
 );
