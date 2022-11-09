d01cb2d0bc580a110e33acda47e06ee2d23c3c3e
Stevo <steven.barrios92@gmail.com>
2022-11-09 00:55:17 -0800

    changed laytou for cards way better, and gotta fix about me section

--- src/components/ContactInfo.js
+++ src/components/ContactInfo.js
@@ -1,64 +1,46 @@
 import { Parallax } from 'react-parallax';
-import firecamp from '../img/backgroundThree 2.jpg'
-import env from '../img/email.png'
+import firecamp from '../img/backgroundThree 2.jpg';
+import env from '../img/email.png';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form'
 
 const ContactInfo = () => (
     <Parallax className='image3' bgImage={firecamp} strength={800}>
         <div className="contact-content" id="contact">
-            <div className="contact-text">
-                <h1 className="text-center"><img src={env} className="env"/>   Contact Me</h1>
-                <form className="px-5 py-1">
-                    <div className="form-group ">
-                        <label for="name" className="py-1 ">
-                            Name:
-                        </label>
-                        <input
-                            value={"name"}
-                            name="name"
-                            onChange={"handleInputChange"}
-                            type="text"
-                            placeholder="name"
-                            class="form-control"
-                        />
-                    </div>
+            <div className='contact-display'>Contanct Me <img src={env}></img></div>
+        <Form>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Text className="text-muted">
+          We'll never share your email with anyone else.
+        </Form.Text>
+      </Form.Group>
 
-                    <div className="form-group">
-                        <label for="email" className="py-1 ">
-                            Email address:
-                        </label>
-                        <input
-                            value={"email"}
-                            name="email"
-                            onChange={"handleInputChange"}
-                            type="email"
-                            placeholder="email"
-                            class="form-control"
-                        />
-                    </div>
-
-                    <div className="form-group">
-                        <label for="message" className="py-1 ">
-                            Message:
-                        </label>
-                        <textarea
-                            value={"message"}
-                            name="message"
-                            onChange={"handleInputChange"}
-                            type="text"
-                            placeholder="message"
-                            class="form-control"
-                            rows="3"
-                        />
-                    </div>
-                    <button
-                        type="submit"
-                        onClick={"handleFormSubmit"}
-                        class="btn btn-primary mt-2"
-                    >
-                        Submit
-                    </button>
-                </form>
-            </div>
+      <Form.Group className="mb-3" controlId="formBasicMessage">
+        <Form.Label>Message</Form.Label>
+        <Form.Control type="message" placeholder="Message" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicCheckbox">
+      </Form.Group>
+      <style type='text/css'>
+        {`
+        .btn-flat {
+            background-color: white;
+            color: black;
+        }
+        .btn-xl {
+            padding: 1rem 1rem;
+            font-size: 1rem;
+        }
+            `}
+      </style>
+      <a href='mailto:steven.barrios92@gmail.com'>
+      <Button variant="flat" type="submit" size='xl' className='border border-danger rounded-pill'>
+        Submit
+      </Button>
+      </a>
+    </Form>
         </div>
     </Parallax>
 );
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,8 +1,5 @@
 import { Parallax } from 'react-parallax';
 import mountain from '../img/woods.jpg';
-import Card from 'react-bootstrap/Card';
-import Col from 'react-bootstrap/Col';
-import Row from 'react-bootstrap/Row';
 import Starwars from '../img/starwars.jpg'
 import Space from '../img/systembade.png';
 import Resume from '../img/resume.jpg';
@@ -10,74 +7,66 @@ import Pines from '../img/nationalPark.jpeg';
 import Bank from '../img/bank.jpeg';
 import Store from '../img/store.jpg'
 
-const cardArray = [
-    {
-        project: 'Space-Invaders',
-        url: 'https://stormy-sands-64982.herokuapp.com/',
-        gbUrl: 'https://github.com/Proper-Stevo/SpaceInvaders',
-        imgSrc: Space,
-        description: 'Space Project'
-    },
-    {
-        project: 'Resume Creator',
-        url: 'https://fierce-ravine-29010.herokuapp.com/',
-        gbUrl: 'https://github.com/cindyung56/resume-creator',
-        imgSrc: Resume,
-        description: 'Resume Creator made fun'
-    },
-    {
-        project: 'Pondersa Pines',
-        url: 'https://proper-stevo.github.io/ThePonderosaPines/',
-        gbUrl: 'https://github.com/Proper-Stevo/ThePonderosaPines',
-        imgSrc: Pines,
-        description: 'A national park finder'
-    },
-    {
-        project: 'Mobile Bank',
-        url: 'https://easybankdemosb.netlify.app',
-        gbUrl: 'https://github.com/Proper-Stevo/EasyBank',
-        imgSrc: Bank,
-        description: 'Front End Mentor Project'
-    }, 
-    {
-        project: 'Store-Redux',
-        url: 'https://floating-reef-85992.herokuapp.com/',
-        gbUrl: 'https://github.com/Proper-Stevo/Store-Redux',
-        imgSrc: Store,
-        description: 'A store that uses Redux to checkout items and utilizes the count measures'
-    },
-    {
-        project: 'Star-Wars API',
-        url: 'https://sb-starwarsapi.netlify.app',
-        gbUrl: 'https://github.com/Proper-Stevo/Star-Wars-API',
-        imgSrc: Starwars,
-        description: 'Star Wars API that gives information on planets, people and ships'
-    }
-]
 
 const Projects = () => (
     <Parallax className='image2' bgImage={mountain} strength={800}>
         <div className='contentProjects' id='work'>
-            <span className='img-txt'>
-                <Row xs={1} md={2} lg={3} className="g-4">
-                    {cardArray.map((item) => (
-                        <Col>
-                            <Card key={item.project} className='uno pop-out-card with-transform'>
-                                <Card.Img variant="top" src={item.imgSrc} className='card-img'/>
-                                <Card.Body className='text-center'>
-                                    <Card.Title>{item.project}</Card.Title>
-                                    <Card.Text className='card-txt'>
-                                        <a class="card-text btn fa-solid fa-earth-americas" href={item.url} > Demo</a>
-                                        <a class="card-text btn fa-brands fa-github" href={item.gbUrl}> github </a>
-                                         <br />
-                                         {item.description}
-                                    </Card.Text>
-                                </Card.Body>
-                            </Card>
-
-                        </Col>
-                    ))}
-                </Row>
+            <span className='img-txt row'>
+                <figure class="snip1256">
+                    <img src={Space} alt="solar system" />
+                    <figcaption>
+                        <h3>Space Odyssey</h3>
+                        <p>A 3D based Solar System, full stack project that includes information on the planets when you select one. </p>
+                        <a href='https://stormy-sands-64982.herokuapp.com/' class="read-more">Demo</a>
+                        <a href='https://github.com/Proper-Stevo/SpaceInvaders' class="read-more">GitHub</a>
+                    </figcaption>
+                </figure>
+                <figure class="snip1256">
+                    <img src={Resume} alt="resume" />
+                    <figcaption>
+                        <h3>Resume Creator</h3>
+                        <p>A React Resume Creator, a simple method to create a resume by answering some questions, fun and interactive. Choose from 4 templates.</p>
+                        <a href='https://fierce-ravine-29010.herokuapp.com/' class="read-more">Demo</a>
+                        <a href='https://github.com/cindyung56/resume-creator' class="read-more">GitHub</a>
+                    </figcaption>
+                </figure>
+                <figure class="snip1256">
+                    <img src={Pines} alt="National Park Badge" />
+                    <figcaption>
+                        <h3>Ponderosa Pines</h3>
+                        <p>A Simple National Park Finder that gives information once you enter a park, give it a try!</p>
+                        <a href='https://proper-stevo.github.io/ThePonderosaPines/' class="read-more">Demo</a>
+                        <a href='https://github.com/Proper-Stevo/ThePonderosaPines' class="read-more">GitHub</a>
+                    </figcaption>
+                </figure>
+                    <br />
+                <figure class="snip1256">
+                    <img src={Bank} alt="Mobile Bank App" />
+                    <figcaption>
+                        <h3>Bank Mobile Responsive</h3>
+                        <p>This is a Front-End Mentor Project that showcases great mobile responsive page.</p>
+                        <a href='https://easybankdemosb.netlify.app' class="read-more">Demo</a>
+                        <a href='https://github.com/Proper-Stevo/EasyBank' class="read-more">GitHub</a>
+                    </figcaption>
+                </figure>
+                <figure class="snip1256">
+                    <img src={Store} alt="store" />
+                    <figcaption>
+                        <h3>Redux Store</h3>
+                        <p>A Redux Store brand that was used to demonstrate the ability to use count and create store.</p>
+                        <a href='https://floating-reef-85992.herokuapp.com/' class="read-more">Demo</a>
+                        <a href='https://github.com/Proper-Stevo/Store-Redux' class="read-more">GitHub</a>
+                    </figcaption>
+                </figure>
+                <figure class="snip1256">
+                    <img src={Starwars} alt="Darth Vader" />
+                    <figcaption>
+                        <h3>Star Wars API</h3>
+                        <p>A Star Wars API that gives information on planets, characters, ships and more!</p>
+                        <a href='https://sb-starwarsapi.netlify.app' class="read-more">Demo</a>
+                        <a href='https://github.com/Proper-Stevo/Star-Wars-API' class="read-more">GitHub</a>
+                    </figcaption>
+                </figure>
             </span></div>
     </Parallax>
 );
