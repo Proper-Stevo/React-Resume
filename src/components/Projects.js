aae76c2617429d0f2a080b231af36b7f41b3ba1b
Stevo <steven.barrios92@gmail.com>
2023-02-25 01:04:58 -0800

    testing code

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -14,36 +14,166 @@ import secondBG from "../img/newBG2.jpg";
 
 function Projects() {
 
-    const { projectsInfo } = projectsData
-    //     // render project image
-    function DisplayProject(project) {
+    // const { projectsInfo } = projectsData
+    // //     // render project image
+    // function DisplayProject(project) {
 
-        const eachProject = Object.values(project);
-        const thisProject = eachProject[0];
+    //     const eachProject = Object.values(project);
+    //     const thisProject = eachProject[0];
 
-        switch (thisProject) {
-            case "Ponderosa Pines":
-                return <Card.Img className="project-img" src={Pines} alt="National Park Project" key="pines"/>;
-            case "Orbital Odyssey":
-                return <Card.Img className="project-img" src={Space} alt="Orbital Odyssey Project" key="space"/>;
-            case "Resume Creator":
-                return <Card.Img className="project-img" src={Resume} alt="Pokemon Data Types" key="resume"/>;
-            case "Pokemon Search":
-                return <Card.Img className="project-img" src={pokeSearch} alt="Pokemon Project" key="pokemon"/>;
-            case "Easy Bank":
-                return <Card.Img className="project-img" src={Bank} alt="Easy Bank Project" key="bank"/>;
-            case "Star Wars API":
-                return <Card.Img className="project-img" src={Starwars} alt="Star wars Project" key="wars" />;
-        }
-    }
+    //     switch (thisProject) {
+    //         case "Ponderosa Pines":
+    //             return <Card.Img className="project-img" src={Pines} alt="National Park Project" key="pines"/>;
+    //         case "Orbital Odyssey":
+    //             return <Card.Img className="project-img" src={Space} alt="Orbital Odyssey Project" key="space"/>;
+    //         case "Resume Creator":
+    //             return <Card.Img className="project-img" src={Resume} alt="Pokemon Data Types" key="resume"/>;
+    //         case "Pokemon Search":
+    //             return <Card.Img className="project-img" src={pokeSearch} alt="Pokemon Project" key="pokemon"/>;
+    //         case "Easy Bank":
+    //             return <Card.Img className="project-img" src={Bank} alt="Easy Bank Project" key="bank"/>;
+    //         case "Star Wars API":
+    //             return <Card.Img className="project-img" src={Starwars} alt="Star wars Project" key="wars" />;
+    //     }
+    // }
+
+    
     return (
         <Parallax className='image2' bgImage={secondBG} strength={800}>
-            <div className='contentProjects' id='work'>
+            <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-4" id="theCards">
+                            <div className="col"> 
+                                <div className="card h-100" id="theCards">
+                                <Card.Img project="Testing" src="https://lorempokemon.fakerapi.it/pokemon/400/110"/>
+                                        <div className="card-body text-center">
+                                            <h5 className="card-title">"Not a test"</h5>
+                                            <p className="card-text">"Or is it?"</p>
+                                            <div className='d-flex text-center justify-content-center'>
+                                            < a href="test2" className='underline'>
+                                                <img src={urlBadge} alt="names"/>
+                                                <p>Website</p>
+                                            </a>
+                                            
+                                            < a href="test3" className='underline'>
+                                                <img src={gbBadge} alt="github tags"/>
+                                                <p>Github</p>
+                                            </a>
+                                            </div>
+                                        </div>
+                                </div>
+                            </div>
+                            <div className="col"> 
+                                <div className="card h-100" id="theCards">
+                                <Card.Img project="Testing" src="https://lorempokemon.fakerapi.it/pokemon/400/110"/>
+                                        <div className="card-body text-center">
+                                            <h5 className="card-title">"Not a test"</h5>
+                                            <p className="card-text">"Or is it?"</p>
+                                            <div className='d-flex text-center justify-content-center'>
+                                            < a href="test2" className='underline'>
+                                                <img src={urlBadge} alt="names"/>
+                                                <p>Website</p>
+                                            </a>
+                                            
+                                            < a href="test3" className='underline'>
+                                                <img src={gbBadge} alt="github tags"/>
+                                                <p>Github</p>
+                                            </a>
+                                            </div>
+                                        </div>
+                                </div>
+                            </div>
+                            <div className="col"> 
+                                <div className="card h-100" id="theCards">
+                                <Card.Img project="Testing" src="https://lorempokemon.fakerapi.it/pokemon/400/110"/>
+                                        <div className="card-body text-center">
+                                            <h5 className="card-title">"Not a test"</h5>
+                                            <p className="card-text">"Or is it?"</p>
+                                            <div className='d-flex text-center justify-content-center'>
+                                            < a href="test2" className='underline'>
+                                                <img src={urlBadge} alt="names"/>
+                                                <p>Website</p>
+                                            </a>
+                                            
+                                            < a href="test3" className='underline'>
+                                                <img src={gbBadge} alt="github tags"/>
+                                                <p>Github</p>
+                                            </a>
+                                            </div>
+                                        </div>
+                                </div>
+                            </div>
+                            <div className="col"> 
+                                <div className="card h-100" id="theCards">
+                                <Card.Img project="Testing" src="https://lorempokemon.fakerapi.it/pokemon/400/110"/>
+                                        <div className="card-body text-center">
+                                            <h5 className="card-title">"Not a test"</h5>
+                                            <p className="card-text">"Or is it?"</p>
+                                            <div className='d-flex text-center justify-content-center'>
+                                            < a href="test2" className='underline'>
+                                                <img src={urlBadge} alt="names"/>
+                                                <p>Website</p>
+                                            </a>
+                                            
+                                            < a href="test3" className='underline'>
+                                                <img src={gbBadge} alt="github tags"/>
+                                                <p>Github</p>
+                                            </a>
+                                            </div>
+                                        </div>
+                                </div>
+                            </div>
+                            <div className="col"> 
+                                <div className="card h-100" id="theCards">
+                                <Card.Img project="Testing" src="https://lorempokemon.fakerapi.it/pokemon/400/110"/>
+                                        <div className="card-body text-center">
+                                            <h5 className="card-title">"Not a test"</h5>
+                                            <p className="card-text">"Or is it?"</p>
+                                            <div className='d-flex text-center justify-content-center'>
+                                            < a href="test2" className='underline'>
+                                                <img src={urlBadge} alt="names"/>
+                                                <p>Website</p>
+                                            </a>
+                                            
+                                            < a href="test3" className='underline'>
+                                                <img src={gbBadge} alt="github tags"/>
+                                                <p>Github</p>
+                                            </a>
+                                            </div>
+                                        </div>
+                                </div>
+                            </div>
+                            <div className="col"> 
+                                <div className="card h-100" id="theCards">
+                                <Card.Img project="Testing" src="https://lorempokemon.fakerapi.it/pokemon/400/110"/>
+                                        <div className="card-body text-center">
+                                            <h5 className="card-title">"Not a test"</h5>
+                                            <p className="card-text">"Or is it?"</p>
+                                            <div className='d-flex text-center justify-content-center'>
+                                            < a href="test2" className='underline'>
+                                                <img src={urlBadge} alt="names"/>
+                                                <p>Website</p>
+                                            </a>
+                                            
+                                            < a href="test3" className='underline'>
+                                                <img src={gbBadge} alt="github tags"/>
+                                                <p>Github</p>
+                                            </a>
+                                            </div>
+                                        </div>
+                                </div>
+                            </div>
+                     </div>
+        </Parallax >
+    );
+};
+
+
+
+ {/* <div className='contentProjects' id='work'>
                 <div>
                     <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-4" id="theCards">
                     {projectsInfo.map((project, index) => {
                         return (
-                            <div className="col">
+                            <div className="col" key={index}> 
                                 <div className="card h-100" id="theCards">
                                 <DisplayProject project={project.title} />
                                         <div className="card-body text-center">
@@ -68,8 +198,6 @@ function Projects() {
                      </div>
                 </div >
             </div >
-        </Parallax >
-    );
-};
+                */}
 
-export default Projects
+                export default Projects
